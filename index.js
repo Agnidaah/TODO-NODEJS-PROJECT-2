e507d2ebbb88dda83b35c882c4920629af4c7783
Agnidaah <108724695+Agnidaah@users.noreply.github.com>
2023-02-09 21:32:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http=require('http');//http handles the request and response
+const url=require('url');
+var ejs = require('ejs');
+let tasks=[];
+http.createServer((req,res)=>{
+    var pathname=url.parse(req.url).pathname;
+    var params = url.parse(req.url,true).query;
+    switch(pathname){
+        case '/Todo':
+            let con=``;
+            if(params.task!="" && params.task!=undefined && tasks[tasks.length-1]!=params.task){tasks.push(params.task)}
+            if(params.tass!=undefined){tasks = tasks.filter(function (task) {
+                return task !== params.tass;
+            });}
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            ejs.renderFile(`public/todo.ejs`,{tas:tasks}, (err, str)=>{con=str});
+            /*^here i was in need of rendering the ejs file and without express i needed to import ejs*/
+            res.end(con);
+        break;
+        default:
+            res.end('URL-localhost:4500/Todo');
+        break;
+    }
+}).listen(4500);
